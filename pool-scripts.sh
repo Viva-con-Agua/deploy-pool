26fc2aca8dec2d3a43a10cc3c0832a9b26a958eb
Dennis Kleber <dennis_kleber@mailbox.org>
2020-01-09 16:38:05 +0100

    version 2020_01_09

--- pool-scripts.sh
+++ pool-scripts.sh
@@ -6,9 +6,22 @@ build_and_setup(){
 restart(){
 	docker-compose rm -s $1 && docker-compose up -d $1;
 }
+
+publish_docker(){
+	docker tag vivaconagua/${1}:latest vivaconagua/${1}:${2}
+  	docker push vivaconagua/${1}:${2};
+}
+
+set_navigation(){
+	echo "set Navigation";
+	curl -X GET http://172.2.100.4:9000/dispenser/navigation/init;
+}
+
 case $1 in
 	update) build_and_setup $2;;
 	restart) restart $2;;
+	publish) publish_docker $2 $3;;
+	initNav) set_navigation;
 esac
 
 
